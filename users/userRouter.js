06767042518ee23ab3e6aaf80f8fd481cd9990f7
harjothkhara <harjoth.khara@gmail.com>
2019-05-29 13:43:54 -0700

    custom middleware functions added

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,12 +1,17 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
+
+//built-in middleware (global)
+router.use(express.json());
+
+//custom middleware
 
 router.post('/', (req, res) => {
 
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
 
 });
 
@@ -14,34 +19,48 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
 
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
 
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    if (!req.params.id) {
+        res.status(400).json({ message: "invalid user id"})
+    } else {
+        req.user = `${req.params.id}`;
+        next();
+    }
 };
 
 function validateUser(req, res, next) {
-
+    if (!req.body) {
+        res.status(400).json({ message: "missing user data"})
+    } else if (!req.body.name) {
+        res.status(400).json({ message: "missing required name field"})
+    }
+    next();
 };
 
 function validatePost(req, res, next) {
-
+    if (!req.body) {
+        res.status(400).json({ message: "missing post data"})
+    } else if (!req.body.text) {
+        res.status(400).json({ message: "missing required text field"})
+    } next();
 };
 
 module.exports = router;
