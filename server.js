8207644581de118a08bf396ce7e4e94b949b5afe
harjothkhara <harjoth.khara@gmail.com>
2019-05-29 13:08:48 -0700

    dependencies added, logger and helmet middleware added (third party), built out custom logger middleware function

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
 
 const server = express();
+
+//built in global middleware - parses body
+server.use(express.json());
+
+//custom middleware
+server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
-
 function logger(req, res, next) {
-
+  const date = new Date();
+  console.log(`(Logger) Request type: ${req.method}, 
+  Request url: ${req.url}, Timestamp: ${date}`)
+  next();
 };
 
 module.exports = server;
